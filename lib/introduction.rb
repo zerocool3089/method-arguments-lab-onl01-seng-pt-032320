7fa47a66d22900dc0ee7ebbf28f342ac88f91c22
Arrington Perry <perryceo21@hotmail.com>
2020-03-24 22:45:01 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,8 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
-def greeting_prgrams(name, programmer)
+def greeting_prgrams(name, language)
 puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
+
   
